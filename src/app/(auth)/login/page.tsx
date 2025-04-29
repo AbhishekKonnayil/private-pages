53610f19c436de4888e3922a45e88a2cd06d319e
Abhishek <abhishek.konnayil@gmail.com>
2025-04-29 15:49:04 +0530

    routing in action

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,58 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-6 ">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+          Login
+        </h1>
+        <form className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              className="w-full border border-gray-300  p-3 mt-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="you@example.com"
+            ></input>
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border border-gray-300  p-3 mt-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="password"
+            ></input>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-blue-500 rounded-lg w-full  py-3 text-white hover:bg-blue-600 transition-colors duration-200"
+            >
+              Login
+            </button>
+          </div>
+          <div className="flex items-center justify-center gap-1">
+            <h1 className="text-gray-600 text-sm">Don't have an account?</h1>
+            <a href="/register" className="text-blue-500 hover:underline">
+              Register
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
